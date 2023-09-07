932ec1f73125cf3f9ed3cde714edb41e8de403a3
dinadendan <dina.dendan222@gmail.com>
2023-09-08 01:00:33 +0200

    refactor and navigate to home

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'dart:ui';
 
 const kPrimaryColor = Color(0xff100B20);
+const kTransitionDuration = Duration(milliseconds: 250);
--- lib/features/splash/presentation/views/widgets/splashViewBody.dart
+++ lib/features/splash/presentation/views/widgets/splashViewBody.dart
@@ -1,6 +1,11 @@
 import 'package:booking_app/core/utils/assets.dart';
+import 'package:booking_app/features/home/presentaion/views/homeView.dart';
 import 'package:booking_app/features/splash/presentation/views/widgets/slidingText.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../../constants.dart';
+
 
 class SplashViewBody extends StatefulWidget{
   const SplashViewBody({Key? key}) : super(key: key);
@@ -18,11 +23,12 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    slidingAnimation = Tween<Offset>(begin: const Offset(0, 5) , end: Offset.zero).animate(animationController) ;
-
-    animationController.forward();
+    initSlidingAnimation();
+    navigateWithTransition();
   }
+
+
+
   @override
   void dispose() {
     super.dispose();
@@ -42,6 +48,19 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
 
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation = Tween<Offset>(begin: const Offset(0, 5) , end: Offset.zero).animate(animationController) ;
+
+    animationController.forward();
+  }
+
+  void navigateWithTransition() {
+    Future.delayed(const Duration(seconds: 3),() {
+      Get.to(() =>const HomeView(),transition: Transition.fadeIn, duration: kTransitionDuration);
+    });
   }
 }
 
